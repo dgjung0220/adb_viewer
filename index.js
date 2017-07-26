69dc0783978ce3d9dbecea17cd15d02cca3e1911
Donggoo Jung <dgjung0220@gmail.com>
2017-07-26 17:52:03 +0900

    first commit / index & package.json

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const electron = require('electron');
+const app = electron.app;
+
+const BrowserWindow = electron.BrowserWindow;
+
+const locals = {};
+const pug = require('electron-pug')({pretty: true, locals});
+
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+var createWindow = () => {
+    mainWindow = new BrowserWindow({width : 800, height : 600});
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/index.pug'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+    
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow();
+    }
+})
